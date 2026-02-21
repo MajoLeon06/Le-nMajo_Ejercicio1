418737cc2c158b69f2b17594933e512992ab8adf
MajoLeon06 <majoleonpaz1321@gmail.com>
2026-02-21 13:33:16 -0600

    Inicio, tipo de atención

--- Ejercicio1/Program.cs
+++ Ejercicio1/Program.cs
@@ -1,1 +1,5 @@
-﻿
+﻿Console.WriteLine("TIPO DE ATENCIÓN");
+Console.WriteLine(" 1: Emergencia");
+Console.WriteLine(" 2: Consulta");
+Console.WriteLine(" 3: Pediatría");
+Console.WriteLine(" 4: Traumatología");
